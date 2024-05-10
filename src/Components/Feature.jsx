996d7a7623dcf2c86b98c389420d5d8acd26aeec
RakibHossen678 <rh491464@gmail.com>
2024-05-10 13:07:09 +0600

    added featured section

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,10 +1,79 @@
-
-const Feature = () => {
-    return (
-        <div>
-            
+const Feature = ({ food }) => {
+  const {
+    donor,
+    foodImg,
+    foodName,
+    foodQuantity,
+    pickupLocation,
+    expiredDate,
+    notes,
+  } = food;
+  console.log(food);
+  return (
+    <div>
+      <div className="flex flex-col max-w-md p-4 space-y-6 overflow-hidden rounded-lg shadow-md ">
+        <div className="flex space-x-4">
+          <img
+            alt=""
+            src={donor.image}
+            className="object-cover w-12 h-12 rounded-full shadow bg-gray-500"
+          />
+          <div className="flex flex-col space-y-1">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="text-sm font-semibold"
+            >
+              {donor.name}
+            </a>
+            <span className="text-xs text-gray-400">{donor.email}</span>
+          </div>
         </div>
-    );
+        <div>
+          <img
+            src={foodImg}
+            alt=""
+            className="object-cover w-full mb-4 h-60 bg-cover bg-center"
+          />
+          <h2 className="mb-1 text-xl font-semibold">{foodName}</h2>
+          <div className="flex py-2 justify-between items-center">
+            <p className="">
+              Expire Date : {new Date(expiredDate).toLocaleDateString()}
+            </p>
+            <p>Quantity : {foodQuantity} servings</p>
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-between">
+          <div className="space-x-2">
+            <button
+              aria-label="Share this post"
+              type="button"
+              className="p-2 text-center"
+            >
+             
+            </button>
+            <button
+              aria-label="Bookmark this post"
+              type="button"
+              className="p-2"
+            >
+             
+            </button>
+          </div>
+          <div className="flex space-x-2 text-sm text-gray-400">
+            <button type="button" className="flex items-center p-1 space-x-1.5">
+              
+              
+            </button>
+            <button type="button" className="flex items-center p-1 space-x-1.5">
+             
+              
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,31 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../Components/Banner";
-
+import Feature from "../Components/Feature";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
+  const foods = useLoaderData();
+  return (
+    <div>
+      <Banner></Banner>
+      <div className="my-20">
+        <div className="max-w-xl mx-auto text-center my-9">
+          <h1 className="text-5xl font-semibold my-3">Featured Foods</h1>
+          <p className="py-3">
+            Explore our curated selection of Featured Foods, meticulously
+            crafted to tantalize your taste buds and ignite your culinary
+            imagination. From gourmet delights to exotic flavors, each dish
+            promises a journey of sensory delight.
+          </p>
         </div>
-    );
+
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 ">
+          {foods.slice(0, 6).map((food) => (
+            <Feature key={food._id} food={food}></Feature>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
